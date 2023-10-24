238afff0a6384f57448f531ecb0e6dfee0827bed
Akinwumidi <israelakin14@gmail.com>
2023-10-24 13:33:13 +0100

    Complete Home section UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,12 @@ const App = () => {
         </Link>
       </header>
 
-      <main></main>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh)-73px]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+      </main>
       <footer></footer>
     </BrowserRouter>
   )
