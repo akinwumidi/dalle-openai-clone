022d03f89de310eda3cbd4ee746581c8526878c7
Akinwumidi <israelakin14@gmail.com>
2023-10-24 12:45:40 +0100

    Complete header section for small and larger devices

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import "./index.css"
+import React from 'react'
+import { Home, CreatePost } from "./pages"
+import { logo } from "./assets"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link to="/" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+          Create
+        </Link>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </BrowserRouter>
   )
 }
 
